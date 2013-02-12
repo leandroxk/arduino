a56142b3159548ef61ef772a72e0c6fc288ff31b
Leandro Corbelo <leandroxk@gmail.com>
2013-02-12 18:11:46 -0200

    Agora utilizando a lib TinyGPS

--- gps_logger/gps_logger.cpp
+++ gps_logger/gps_logger.cpp
@@ -1,10 +1,14 @@
 #include "gps_logger.h"
 #include <HardwareSerial.h>
 #include <SD.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
 extern HardwareSerial Serial;
 
 const char* GPS_DIR = "/GPS";
+
+TinyGPS gps;
 
 void setup() {
 	Serial.begin(9600);
@@ -22,43 +26,6 @@ void setup() {
 	gps_dir.close();
 }
 
-int nmea_checksum(char *s) {
-	int c = 0;
-	while (*s) {
-		c ^= *s++;
-	}
-
-	return c;
-}
-
-void build_nmea_string(char* text) {
-	strcpy(text, "$GPRMC");
-	strcat(text, ",");
-	strcat(text, "092750.000");
-	strcat(text, ",");
-	strcat(text, "A");
-	strcat(text, "5321.6801");
-	strcat(text, ",");
-	strcat(text, "N");
-	strcat(text, ",");
-	strcat(text, "00630.3372");
-	strcat(text, ",");
-	strcat(text, "W");
-	strcat(text, ",");
-	strcat(text, "0.02");
-	strcat(text, ",");
-	strcat(text, "31.66");
-	strcat(text, ",");
-	strcat(text, "280511");
-	strcat(text, ",,,");
-	strcat(text, "A*");
-
-	int checksum = nmea_checksum(text);
-	char buf[sizeof(checksum) + 1];
-	itoa(checksum, buf, 10);
-	strcat(text, buf);
-}
-
 File get_log_file() {
 	char path[strlen(GPS_DIR) + 14];
 	strcpy(path, GPS_DIR);
@@ -71,12 +38,10 @@ File get_log_file() {
 void loop() {
 	File gps_log = get_log_file();
 
-	char mnea[90];
-	build_nmea_string(mnea);
-
+	const char* mnea = "$GPRMC,092750.000,A,5321.6802,N,00630.3372,W,0.02,31.66,280511,,,A*43";
 	gps_log.println(mnea);
 	gps_log.close();
-	Serial.println(mnea);
+
 
 	delay(1000);
 }
